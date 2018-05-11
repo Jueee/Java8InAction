360a3dad27bc4c5fd1b8e059146b24c84a43b1d3
尉勇强2 <hzweiyongqiang@corp.netease.com>
2018-05-11 19:38:54 +0800

    add init files

---
+++ src/test/java/com/java8/action/chapter01/Course10WhyChangingTest.java
@@ -0,0 +1,13 @@
+package com.java8.action.chapter01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Course10WhyChangingTest {
+
+	@Test
+	public void test() {
+	}
+
+}
